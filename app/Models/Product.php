09b115b7a40d98e44b0e8333a85cfdfc97dd3bee
nazaninns <nazaninnsq@gmail.com>
2024-07-28 21:10:42 +0330

    update relations in models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -18,5 +20,15 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems():HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function featureOptions():BelongsToMany
+    {
+        return $this->belongsToMany(FeatureOption::class);
     }
 }
