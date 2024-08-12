b58b5223cb2d41de7fec0609a4b6980ac6df4cd6
nazaninns <nazaninnsq@gmail.com>
2024-08-12 22:27:04 +0330

    repclace laravel ui with breeze for update project

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
