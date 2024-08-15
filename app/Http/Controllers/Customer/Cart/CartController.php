20bb7006fbab051c430436e87579e8fbd1507040
nazaninns <nazaninnsq@gmail.com>
2024-08-15 17:26:46 +0330

    add resource for response cart

--- app/Http/Controllers/Customer/Cart/CartController.php
+++ app/Http/Controllers/Customer/Cart/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer\Cart;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Customer\Cart\AddProductToCartRequest;
 use App\Http\Requests\Customer\Cart\DeleteProductToCartRequest;
+use App\Http\Resources\Customer\Cart\CartResource;
 use App\Services\CartService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -19,14 +20,14 @@ class CartController extends Controller
         $cartService = new CartService($request->user()->id);
         $cartService->addProduct($data['product_id'], $data['quantity']);
         $cart = $cartService->get();
-        return response()->json(['message' => 'ok', 'data' => $cart]);
+        return response()->json(['message' => 'ok', 'data' => CartResource::make($cart)]);
     }
 
 
     public function showCart(): JsonResponse
     {
         $cart = Cache::get('cart_' . Auth::id());
-        $data = $cart ?? [];
+        $data = CartResource::make($cart) ?? [];
         return response()->json(['message' => 'ok', 'data' => $data]);
     }
 
@@ -36,6 +37,6 @@ class CartController extends Controller
         $cartService = new CartService($request->user()->id);
         $cartService->deleteProduct($data['product_id'], $data['quantity']);
         $cart = $cartService->get();
-        return response()->json(['message' => 'ok', 'data' => $cart]);
+        return response()->json(['message' => 'ok', 'data' => CartResource::make($cart)]);
     }
 }
