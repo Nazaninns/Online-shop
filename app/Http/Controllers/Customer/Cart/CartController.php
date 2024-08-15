d4136b18e869b3b595a8a0bcc60df985cd964e72
nazaninns <nazaninnsq@gmail.com>
2024-08-15 11:58:00 +0330

    add controller,route for cart

---
+++ app/Http/Controllers/Customer/Cart/CartController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Customer\Cart;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function addProductToCart()
+    {
+        
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
