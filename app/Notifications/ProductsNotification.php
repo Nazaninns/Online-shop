2da394f8e781feff5dd2ef90fd2e1cb430104171
nazaninns <nazaninnsq@gmail.com>
2024-09-04 16:40:30 +0330

    add command for send email to customers

---
+++ app/Notifications/ProductsNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Collection;
+
+class ProductsNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Collection $products)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('this products have discount!')
+            ->lines($this->products->map(function (Product $product) {
+                return 'name: ' . $product->name . PHP_EOL . 'price: ' . $product->price;
+            }))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
